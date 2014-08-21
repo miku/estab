a443288a46813cfe270eebf5f9ffb6448df29c07
Martin Czygan <martin.czygan@gmail.com>
2014-08-21 22:39:03 +0200

    can display 10 items

--- cmd/estab/estab.go
+++ cmd/estab/estab.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
+	"strings"
 
 	"github.com/belogik/goes"
 )
@@ -11,10 +14,46 @@ func main() {
 
 	host := flag.String("host", "localhost", "elasticsearch host")
 	port := flag.String("port", "9200", "elasticsearch port")
+	indicesString := flag.String("indices", "", "indices to search (or all)")
+	fieldsString := flag.String("f", "content.245.a", "field or fields space separated")
 
 	flag.Parse()
 
+	var indices []string
+	trimmed := strings.TrimSpace(*indicesString)
+	if len(trimmed) > 0 {
+		indices = strings.Fields(trimmed)
+	}
+
+	fields := strings.Fields(*fieldsString)
 	conn := goes.NewConnection(*host, *port)
-	fmt.Println(conn)
+	var query = map[string]interface{}{
+		"query": map[string]interface{}{
+			"match_all": map[string]interface{}{},
+		},
+		"fields": fields,
+	}
+
+	extraArgs := make(url.Values, 1)
+	searchResults, err := conn.Search(query, indices, []string{""}, extraArgs)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, hit := range searchResults.Hits.Hits {
+		var columns []string
+		for _, f := range fields {
+			switch value := hit.Fields[f].(type) {
+			case []interface{}:
+				var c []string
+				for _, e := range value {
+					c = append(c, e.(string))
+				}
+				columns = append(columns, strings.Join(c, "|"))
+			default:
+				log.Fatal("unknown field type in response")
+			}
+		}
+		fmt.Println(strings.Join(columns, "\t"))
+	}
 
 }
